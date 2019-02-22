9bb9c4139b01ad095b4cd8598e7970c177043d7d
CybrChick007 <37068838+CybrChick007@users.noreply.github.com>
2019-02-22 09:39:31 +0000

    Working Server for static pages

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./pages'));
+app.use(express.static('pages'));
 
-app.listen(8080);
+
+
+app.listen(8080, () => console.log('Listing on port 8080'));
