350a121382d472592e3499b7c37d5c8d5708cfa6
CybrChick007 <37068838+CybrChick007@users.noreply.github.com>
2019-03-13 11:01:32 +0000

    :art: cosmetic changes

---
+++ pages/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+
+
+
+
+// document.body.onload = laneList;
+//
+// function laneList() {
+//   for (let i = 1; 1 < 21; i++) {
+//
+//
+//     let newLi = document.createElement("li");
+//
+//     let newLane = document.createTextNode("Lane" + i + ": ");
+//
+//     newLi.appendChild(newLane);
+//
+//     let currentLi = document.getElementById("laneList");
+//     document.body.appendChild(newLi, currentLi);
+//   }
+//
+// }
