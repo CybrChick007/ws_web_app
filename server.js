99f1fd789fbc0a463f465fc44a274e659ddfd7a8
CybrChick007 <37068838+CybrChick007@users.noreply.github.com>
2019-03-15 12:39:47 +0000

    Server and package updates

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 'use static';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const api = require('./api');
+
 const app = express();
+app.use(bodyParser.json());
+
+// app.use(api);
 
 app.use(express.static('pages'));
-
-
 
 app.listen(8080, () => console.log('Listing on port 8080'));
